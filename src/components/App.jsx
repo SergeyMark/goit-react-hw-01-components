13f49bdf1511cfe9c492fec3e6ee005c0238df7e
Sergey <110375004+SergeyMark@users.noreply.github.com>
2023-03-02 20:50:57 +0200

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,18 @@
+import user from '../user.json'
+import { Profile } from './Profile/Profile';
+
 export const App = () => {
   return (
-    <div>Hello</div>
+    <>
+      <Profile 
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </>
   );
 };
